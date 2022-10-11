d4415e46a016536d427519abb063424c3ff9c672
vigi25022000 <114006935+vigi25022000@users.noreply.github.com>
2022-10-11 21:01:07 +0530

    Add files via upload

---
+++ lab_sesh2/src/com/greatlearning/service/NotesCount.java
@@ -0,0 +1,31 @@
+package com.greatlearning.service;
+
+public class NotesCount {
+	
+	public void notesCountImplementation(int notes[], int amount) {
+		
+		int[] noteCounter = new int[notes.length];
+		
+		try {
+			for (int i=0; i<notes.length; i++) {
+				if (amount>=notes[i]) {
+					noteCounter[i] = amount/notes[i];
+					amount = amount - noteCounter[i]*notes[i];
+			
+				}
+			}
+			if (amount>0) {
+				System.out.println("exact amount to be given with the highest denomination");
+			}else {
+				System.out.println ("Your payment approach in order to give min no of notes will be");
+				for (int i=0; i<notes.length; i++) {
+					if (noteCounter[i] != 0) {
+						System.out.println(notes[i] + ":" + noteCounter[i]);
+					}
+				}
+			}
+		} catch (ArithmeticException e) {
+			System.out.println(e+ " notes of denomination of 0 is invalid");
+		}
+	}
+}	 
